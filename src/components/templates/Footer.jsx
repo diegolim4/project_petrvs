2bc9dc64a03fd426ed03ec2ae76f12d50a0ad777
Diego <lima.j.diego@gmail.com>
2021-07-28 09:55:17 -0300

    Criando components home, header, footer

---
+++ src/components/templates/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+export default ()=>{
+    return(
+        <>
+            <footer className="foot">
+                Developer Diego Lima
+            </footer>
+        </>
+    )
+}
---
+++ src/components/templates/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+
+
+export default () => {
+    return(
+        <>
+            <Header />
+            <h1>Bem vindo!</h1>
+            <Footer />
+        </>
+    )
+    
+}
