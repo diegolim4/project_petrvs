d81ceefb668c7401e27d1feabe9d1b796a28608d
Diego <lima.j.diego@gmail.com>
2021-07-29 07:48:43 -0300

    criado component views

---
+++ src/components/views/templates/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Body.css'
+
+export default () => {
+    return(
+        <body className="body"> 
+            <h2>Title</h2>         
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure debitis accusamus repellendus blanditiis aut? Necessitatibus, consequatur accusamus harum, totam a tenetur recusandae illum delectus molestias suscipit, laborum corporis eos tempore.</p>            
+                               
+        </body>
+    )
+    
+}
---
+++ src/components/views/templates/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+import Body from './Body'
+
+
+export default () => {
+    return(
+        <>
+            <Header />
+            <Body />
+            <Footer />
+        </>
+    )
+    
+}
